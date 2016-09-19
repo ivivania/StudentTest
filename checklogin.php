5d58d4ba749637d40aedeb3c6a6a38c95d5c24b9
ivivania <ivi_vania@abv.bg>
2016-09-19 13:16:42 +0300

    Add files via upload

---
+++ checklogin.php
@@ -0,0 +1,60 @@
+<?php
+
+	include_once("db/connection.inc.php");
+	include_once("db/mysql_database.inc.php");
+
+// предаване на параметрите username and password от входящата форма 
+	$myusername=$_POST['myusername'];
+	$mypassword=$_POST['mypassword']; 
+
+//създаване на обект към DB
+	$db=DBFactory::CreateDatabaseObject("MySqlDatabase");
+	$db->Connect($server, $database, $username, $password);
+
+	$dt=$db->Execute("SELECT UserID, CategoryID FROM Users WHERE UserName='$myusername' AND Password='$mypassword'");
+	
+	//проверка за наличие на записи в БД
+	$count=$dt->Count();
+	if($count==1){
+		while($dt->MoveNext())
+		{
+			$par=$dt->__get('CategoryID');
+			$user_id=$dt->__get('UserID');
+			
+		}	
+	}
+	else {
+		echo "Грешна парола или потребител!";
+		header("location:error_index.php");
+	}
+	
+	
+//проверява се върнатия резултат от заявката към БД и се определя към кой модул да се премине
+	switch(($par))
+	{
+		case 0:
+			throw new Exception("Error");
+			header("location:error_index.php");
+		case 1:
+			//echo "Вие сте студент!";					
+			header("Location: student/index.php?id=$user_id");
+			break;
+		case 2:
+			//echo "Вие сте учител!";						
+			header("location:teacher/index.php");
+			break;
+		case 3:
+			echo "Администратор!";					
+			header("location:admin/index.php?id=$user_id");
+			break;
+		case 4:
+			echo "Ваня е тук!";					
+			header("location:root/index.php");
+			break;
+
+		default:
+			echo "Opa!";
+			header("location:error_index.php");		
+			break;		
+	}	
+?>
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php ?>
+<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+
+<table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+<tr>
+<form name="form1" method="post" action="checklogin.php">
+<td>
+<table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
+<tr>
+<td colspan="3"><strong>Вход в системата</strong></td>
+</tr>
+<tr>
+<td width="78">потребител</td>
+<td width="6">:</td>
+<td width="294"><input name="myusername" type="text" id="myusername"></td>
+</tr>
+<tr>
+<td>парола</td>
+<td>:</td>
+<td><input name="mypassword" type="password" id="mypassword"></td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td>&nbsp;</td>
+<td><input type="submit" name="Submit" value="Вход"></td>
+</tr>
+</table>
+</td>
+</form>
+</tr>
+</table>
